6d74bca888cea09466fd40b222dbd0c1fd8ad172
Nicholas Chrysanthou <nchrysanthou@mtrx-node05pd.dcm.senecacollege.ca>
2020-10-01 01:14:06 -0400

    Final lab project

--- temps.c
+++ temps.c
@@ -5,10 +5,7 @@
 // Workshop:       3 (in-lab)
 //==============================================
 
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
-#include <stdbool.h>
 
 #define NUMS 3
 
@@ -16,7 +13,6 @@ int main(void)
 {
 
     printf("---=== IPC Temperature Analyzer ===---\n");
-    bool valid;
     int low, high, i;
 
     for (i = 1; i <= NUMS; i++)
@@ -25,17 +21,19 @@ int main(void)
         {
             printf("Enter the high value for day %d: ", i);
             scanf("%d", &high);
-            printf("\n");
+	           printf("\n");
             printf("Enter the low value for day %d: ", i);
             scanf("%d", &low);
-            printf("\n");
-
-            if (!(high > low && high <= 40 && low >= -40))
+                                printf("\n");
+            
+	           if (!(high > low && high <= 40 && low >= -40))
             {
                 printf("Incorrect values, temperatures must be in the range -40 to 40, high must be greater than low.\n");
-                i--;
+       printf("\n");
+
             }
-        } while (valid);
+else { i++;}
+        } while (i <= NUMS);
     }
 
     return 0;
