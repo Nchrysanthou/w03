2aa4d0dbcffb69dbcfd49ca6c518627221bf0e6c
Nicholas Chrysanthou <Nchrysanthou@myseneca.ca>
2020-10-01 00:20:24 -0400

    added lab workshop

--- temps.c
+++ temps.c
@@ -4,26 +4,41 @@
 // Email:          Nchrysanthou@myseneca.ca
 // Workshop:       3 (in-lab)
 //==============================================
-
+#include <stdbool.h>
 #include <stdio.h>
 #define NUM 3
 
 int main(void)
 {
     printf("---=== IPC Temperature Analyzer ===---\n");
-
+    bool valid;
     int high, low;
 
-    for (int i = 0; i < NUM; i++)
+    for (int i = 1; i <= NUM; i++)
     {
         do
         {
-            printf("Enter the high value for day %d:\n", i + 1);
+            printf("Enter the high value for day %d:\n", i);
             scanf("%d", &high);
-            printf("Enter the low value for day %d:\n", i + 1);
+            printf("Enter the low value for day %d:\n", i);
             scanf("%d", &low);
-        } while ();
-    }
 
+            if (low < -40 || high > 40)
+            {
+                printf("incorrect values, temperatures must be in the range of -40 to 40, high must be greater than low\n");
+                valid = false;
+            }
+
+            else if (low < high)
+                printf("incorrect values, temperatures must be in the range of -40 to 40, high must be greater than low\n");
+
+            else
+            {
+                valid = true;
+                printf("your value is correct\n");
+            }
+
+        } while (!valid);
+    }
     return 0;
 }
